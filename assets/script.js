dcb74c67f52d23bd7b9a10b8b3123e7c698e50e5
Hiba Ali <hiba.mumin22@gmail.com>
2022-04-17 12:44:26 -0500

    added variable for date and made buttons interactive

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,21 @@
-function getWeather() {
-    var newCity = document.getElementById("cityInput");
-    var cityName = document.getElementById("cityName");
-    cityName.innerHTML = "--"+newCity.value+"--"
+// Variable for Date
+var date = new Date();
+var month = date.getMonth() + 1;
+var day = date.getDate();
+var year = date.getFullYear();
+var today = month + "-" + day + "-" + year;
 
-   
+var savedButton = $(".saved-button");
+var searchButton = $("#searchButton");
+var searchInput = $("#searchInput");
+var cityName = "";
+var pastSearches = [];
+
+function searchHistory() {
+  var savedCities = $("#savedCities");
+  var trEl = $("<tr>");
+  var thEl = $("<th>");
+  thEl.text(cityName).attr("scope", "row").addClass("saved-button");
+  trEl.append(thEl);
+  savedCities.append(trEl);
 }
